aad60690bcd6b82d02f79cf94015587a5af5bb5c
moscugeorge123 <geodasilva123@gmail.com>
2020-02-27 23:10:35 +0200

    architecture

---
+++ src/app/timetable/components/course-time/course-time.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CourseTimeComponent } from './course-time.component';
+
+describe('CourseTimeComponent', () => {
+  let component: CourseTimeComponent;
+  let fixture: ComponentFixture<CourseTimeComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CourseTimeComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CourseTimeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/timetable/components/course-time/course-time.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course-time',
+  templateUrl: './course-time.component.html',
+  styleUrls: ['./course-time.component.scss']
+})
+export class CourseTimeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
