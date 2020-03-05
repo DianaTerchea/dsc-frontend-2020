f6fc344884302e8dbd87a7f8b26781d6b1b43871
Serghei Cunev <serghei.cunev@gmail.com>
2020-03-05 19:19:21 +0200

    Added a form for login with email and password

--- src/app/session/pages/login/login.component.ts
+++ src/app/session/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder,Validator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
   }
+
+  onSubmit() {
+    console.warn(this.profileForm.value);
+  }
+
+  profileForm = this.fb.group({
+    email:['', [Validators.required, Validators.email]],
+    password:['', Validators.required],
+  })
 
 }
