8e424d717caa063bf1f330c81c4def48a5098efb
Flavius <aflaviusandrei@gmail.com>
2020-03-25 09:15:19 +0200

    Days now stay picked even when clicking outside

--- src/app/timetable/components/week-days/week-days.component.ts
+++ src/app/timetable/components/week-days/week-days.component.ts
@@ -9,6 +9,13 @@ import { TimeTableService } from '../../services/time-table.service';
 export class WeekDaysComponent {
   constructor(private _timetableService: TimeTableService) {}
   updateDay(day) {
+    if (document.getElementsByClassName('picked-day').length)
+      document
+        .getElementsByClassName('picked-day')[0]
+        .classList.remove('picked-day');
+    document
+      .getElementById(day.substring(0, 3).toLowerCase())
+      .classList.add('picked-day');
     this._timetableService.sendMessage(day);
   }
 }
